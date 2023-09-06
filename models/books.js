586e6bdc2174d3e1c20782f76bf126be0b44089e
FlorRoth <roth.florencia99@gmail.com>
2023-09-06 14:52:38 -0300

    create model

---
+++ models/books.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, type) => {
+
+    return sequelize.define('book',{
+        id: {
+            type: type.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: type.STRING,
+        author: type.STRING,
+        description: type.STRING,
+        pages: type.INTEGER
+    })
+}
