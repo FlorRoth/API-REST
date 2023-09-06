e185e1c5236b389cbd209f8db0d95f3c39986c73
FlorRoth <roth.florencia99@gmail.com>
2023-09-06 14:53:48 -0300

    update index

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-import express  from "express";
+const express= require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+require ('./db');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/autores', (req, res) => res.send('prueba obteniendo autores'));
+
+app.post('/autores', (req, res) => res.send('prueba creando autores'));
+
+app.put('/autores', (req, res) => res.send('prueba actualizando autores'));
+
+app.delete('/autores', (req, res) => res.send('prueba eliminando autores'));
 
 app.listen(3000);
